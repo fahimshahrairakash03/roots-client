feebf61f80e1fdd0b8a491b050cba0bd269ea2ed
Fahim Shahriar Akash <fahimshahriarakash03@gmail.com>
2022-11-08 19:12:35 +0600

    signin with email added

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -1,5 +1,10 @@
-import React, { createContext, useState } from "react";
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import React, { createContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import app from "../firebase/firebase.config";
 
 export const AuthContext = createContext();
@@ -13,7 +18,21 @@ const AuthProvider = ({ children }) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
-  const authinfo = { user, signup };
+  const signin = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      console.log(currentUser);
+    });
+    return () => {
+      return unsubscribe();
+    };
+  }, []);
+
+  const authinfo = { user, signup, signin };
 
   return (
     <AuthContext.Provider value={authinfo}>{children}</AuthContext.Provider>
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
 import login from "../../assets/login.jpg";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/AuthProvider";
 
 const Login = () => {
+  const { signin } = useContext(AuthContext);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
+    signin(email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        form.reset();
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
     <div className="row  container mb-5 px-5 ">
       <div className="col-lg-6 col-sm-12 mx-auto">
@@ -12,20 +30,23 @@ const Login = () => {
       </div>
       <div className="col-lg-6 col-sm-12 my-auto mx-auto ">
         <h1>Please Login</h1>
-        <Form>
+        <Form onSubmit={handleLogin}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control type="email" name="email" placeholder="Enter email" />
           </Form.Group>
-
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control
+              type="password"
+              name="password"
+              placeholder="Password"
+            />
           </Form.Group>
-
-          <Button variant="primary" type="submit">
+          <Button className="me-3" variant="primary" type="submit">
             Submit
           </Button>
+          <Button variant="outline-success">SignIn With Google</Button>{" "}
         </Form>
         <p>
           Do not have an Account? <Link to="/signup"> Signup </Link>{" "}
